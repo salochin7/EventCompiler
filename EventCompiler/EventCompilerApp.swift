31c45cf82d08f758cb9ee70d1463db6f2b64a185
Nicholas Noranbrock <nicholas@noranbrock.com>
2025-04-16 15:08:29 -0400

    Initial Commit

---
+++ EventCompiler/EventCompilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCompilerApp.swift
+//  EventCompiler
+//
+//  Created by Nicholas Noranbrock on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct EventCompilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EventCompilerTests/EventCompilerTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventCompilerTests.swift
+//  EventCompilerTests
+//
+//  Created by Nicholas Noranbrock on 4/16/25.
+//
+
+import Testing
+@testable import EventCompiler
+
+struct EventCompilerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
